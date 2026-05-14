755cc637c41eba00d7d2403d093528f60bad53c7
Thomas Vogt <acc-github@tovotu.de>
2026-05-14 20:07:15 +0200

    panelVisibilityManager: remove _tween, it's been obsolete since GNOME 3.34

--- panelVisibilityManager.js
+++ panelVisibilityManager.js
@@ -66,26 +66,15 @@ export class PanelVisibilityManager {
         // We lost the original notification's position because of
         // PanelBox->affectsStruts = false and now it appears beneath the
         // top bar, fix it
-        if (!!MessageTray._tween) {
-            this._oldTween = MessageTray._tween;
-            MessageTray._tween = (
-                function(actor, statevar, value, params) {
-                    params.y += (PanelBox.y < 0 ? 0 : PanelBox.height);
-                    this._oldTween.apply(MessageTray, arguments);
+        this._oldEase = MessageTray._bannerBin.ease;
+        MessageTray._bannerBin.ease = (
+            function(params) {
+                if (params.hasOwnProperty("y") && PanelBox.y >= 0) {
+                    params.y += PanelBox.height;
                 }
-            ).bind(this);
-        } else {
-            // under more recent versions of GNOME, _tween has been replaced by other functions
-            this._oldEase = MessageTray._bannerBin.ease;
-            MessageTray._bannerBin.ease = (
-                function(params) {
-                    if (params.hasOwnProperty("y") && PanelBox.y >= 0) {
-                        params.y += PanelBox.height;
-                    }
-                    this._oldEase.apply(MessageTray._bannerBin, arguments);
-                }
-            ).bind(this);
-        }
+                this._oldEase.apply(MessageTray._bannerBin, arguments);
+            }
+        ).bind(this);
 
         this._pointerWatcher = PointerWatcher.getPointerWatcher();
         this._pointerListener = null;
@@ -557,11 +546,7 @@ export class PanelVisibilityManager {
           _searchEntryBin.style = null;
         }
 
-        if (!!MessageTray._tween) {
-            MessageTray._tween = this._oldTween;
-        } else {
-            MessageTray._bannerBin.ease = this._oldEase;
-        }
+        MessageTray._bannerBin.ease = this._oldEase;
         this.show(0, "destroy");
 
         Main.layoutManager.removeChrome(PanelBox);
